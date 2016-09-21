e4be2ac4f77f9e84ccc4f1d83d95f58b41a98836
Yvonne Chan <ychan1352@gmail.com>
2016-09-21 11:56:26 -0400

    please update not sure if works correctly

--- sampleflask.py
+++ sampleflask.py
@@ -1,19 +1,22 @@
 from flask import Flask
 
-app = Flask(__name__) 
+sampleflask = Flask(__name__) 
 
-@app.route("/")
+@sampleflask.route("/")
 def home():
-    return "Do you like cats or dogs more?"
+    return '''Do you like cats or dogs more? <br>
+<a href="http://127.0.0.1:5000/cats">Cats</a>
+<a href="http://127.0.0.1:5000/dogs">Dogs</a>'''
 
-@app.route("/cats")
+@sampleflask.route("/cats")
 def cat():
-    return "The choice is obvious. Cats are purrfect."
+    return '''The choice is obvious. Cats are purrfect.<br>
+<a href="http://127.0.0.1:5000/">Change your mind?</a>'''
 
-@app.route("/dogs")
+@sampleflask.route("/dogs")
 def dog():
-    return "This shouldn't be a pawblem. Dogs are the best."
+    return '''This shouldn't be a pawblem. Dogs are the best.<br>
+<a href="http://127.0.0.1:5000/">Change your mind?</a>'''
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    sampleflask.run()
